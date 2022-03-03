157745ad0f4da483b387f3e81d9afd062c86af9c
Pablo M. Fernandez <pfernandezdonofrio@santandertecnologia.com.ar>
2022-03-03 11:32:50 -0300

    *** Primer commit +++

---
+++ src/test/java/com/hateoas/poc/PocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hateoas.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
